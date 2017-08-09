6933229baf27a80ddfee83afd5144fab87d4b9bb
Wen-Long Yang <matesea@gmail.com>
2017-08-09 10:26:56 +0800

    bashrc: add cdf

--- bash/fzf.sh
+++ bash/fzf.sh
@@ -14,3 +14,9 @@ fd() {
   cd "$dir"
 }
 
+# cdf - cd into the directory of the selected file
+cdf() {
+   local file
+   local dir
+   file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
+}
