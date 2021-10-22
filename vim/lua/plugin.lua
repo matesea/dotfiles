15fd6dbf6674e033614ae2cde85dacc2edf68b05
Wen-Long Yang <matesea@gmail.com>
2021-10-22 17:27:45 +0800

    vimrc: switch to lualine

--- vim/lua/plugin.lua
+++ vim/lua/plugin.lua
@@ -122,7 +122,7 @@ require('packer').startup{function()
         statusline.ale_diagnostics = false
         --]]
 
-        use {'hoob3rt/lualine.nvim',
+        use {'nvim-lualine/lualine.nvim',
             requires = {'kyazdani42/nvim-web-devicons', opt = true},
             config = function()
                 require('lualine').setup{
