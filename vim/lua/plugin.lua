dc7d08b8b968ba2f0aef10ec85bc99b8bd22cd9d
Wen-Long Yang <matesea@gmail.com>
2022-05-02 15:48:38 +0800

    ff: move indexed files into .files.gz

--- vim/lua/plugin.lua
+++ vim/lua/plugin.lua
@@ -280,18 +280,49 @@ require('packer').startup{function()
 
         use { 'ibhagwan/fzf-lua',
             disable = true,
-            requires = {
-                {'vijaymarupudi/nvim-fzf',
-                    opt = true,
-                },
-                {'kyazdani42/nvim-web-devicons',
-                    opt = true,
-                },
-            },
             config = function()
-                vim.api.nvim_set_keymap('n', '<space>e', "<cmd>lua require('fzf-lua').files({files = {cmd = '(global -Pol || rg --no-messages --files --no-ignore) 2>/dev/null'}})<cr>",
-                    {noremap = true, silent = true})
-                vim.api.nvim_set_keymap('n', '<space>c', "<cmd>lua require('fzf-lua').files({cwd = %:h, files = {cmd = '(global -Pol || rg --no-messages --files --no-ignore) 2>/dev/null'}})<cr>",
+                require('fzf-lua').setup{
+                    files = {
+                        cmd = 'ff',
+                        git_icons = false,
+                        file_icons = false,
+                        color_icons = false,
+                        previewer = false,
+                    },
+                    git = {
+                        files = {
+                            git_icons = false,
+                            file_icons = false,
+                            color_icons = false,
+                            previewer = false,
+                        },
+                        status = {
+                            git_icons = false,
+                            file_icons = false,
+                            color_icons = false,
+                        },
+                    },
+                    grep = {
+                        git_icons = false,
+                        file_icons = false,
+                        color_icons = false,
+                    },
+                    tags = {
+                        git_icons = false,
+                        file_icons = false,
+                        color_icons = false,
+                    },
+                    btags = {
+                        git_icons = false,
+                        file_icons = false,
+                        color_icons = false,
+                    },
+                    quickfix = {
+                        git_icons = false,
+                        file_icons = false,
+                    },
+                }
+                vim.api.nvim_set_keymap('n', '<space>e', '<cmd>FzfLua files<cr>',
                     {noremap = true, silent = true})
                 vim.api.nvim_set_keymap('n', '<space>rg', "<cmd>FzfLua live_grep<cr>",
                     {noremap = true, silent = true})
@@ -299,14 +330,15 @@ require('packer').startup{function()
                     {noremap = true, silent = true})
                 vim.api.nvim_set_keymap('n', '<space>b', "<cmd>FzfLua buffers<cr>",
                     {noremap = true, silent = true})
-                -- vim.api.nvim_set_keymap('n', '<space>hc', "<cmd>FzfLua command_history<cr>",
-                --     {noremap = true, silent = true})
-                -- vim.api.nvim_set_keymap('n', '<space>hf', "<cmd>FzfLua oldfiles<cr>",
-                --     {noremap = true, silent = true})
-                -- vim.api.nvim_set_keymap('n', '<space>hs', "<cmd>FzfLua search_history<cr>",
-                --     {noremap = true, silent = true})
+                vim.api.nvim_set_keymap('n', '<space>hc', "<cmd>FzfLua command_history<cr>",
+                    {noremap = true, silent = true})
+                vim.api.nvim_set_keymap('n', '<space>hf', "<cmd>FzfLua oldfiles<cr>",
+                    {noremap = true, silent = true})
+                vim.api.nvim_set_keymap('n', '<space>hs', "<cmd>FzfLua search_history<cr>",
+                    {noremap = true, silent = true})
                 vim.api.nvim_set_keymap('n', '<space>a', "<cmd>FzfLua lines<cr>",
                     {noremap = true, silent = true})
+                vim.cmd[[nnoremap <space>c :FzfLua files cwd=<C-R>=expand("%:h")<cr><cr>]]
             end
         }
 
