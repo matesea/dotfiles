dceb69fb68279da0afc4531e7f211bc35167801d
wenlongy <wenlongy@qti.qualcomm.com>
2022-05-18 15:24:41 +0800

    vimrc: show fileformat of this buffer in statusline

--- vim/lua/statusline/init.lua
+++ vim/lua/statusline/init.lua
@@ -2,7 +2,8 @@
   local mode = "%-5{%v:lua.string.upper(v:lua.vim.fn.mode())%}"
   local file_name = "%-.16t"
   -- local buf_nr = "[%n]"
-  local fileencoding = " [%{&fileencoding?&fileencoding:&encoding}]"
+  local fileformat = " [%{&fileformat}/"
+  local fileencoding = "/%{&fileencoding?&fileencoding:&encoding}]"
   local modified = " %-m"
   local file_type = " %y"
   local right_align = "%="
@@ -10,12 +11,13 @@
   local pct_thru_file = "%5p%%"
 
   return string.format(
-    "%s%s%s%s%s%s%s%s",
+    "%s%s%s%s%s%s%s%s%s",
     mode,
     file_name,
     modified,
     file_type,
     right_align,
+    fileformat,
     fileencoding,
     line_no,
     pct_thru_file
