67b3d3ae09043c63daadc193bc7ed9a33f18848d
Wen-Long Yang <matesea@gmail.com>
2022-10-04 14:04:59 +0800

    vimrc: enable fzf-lua for optional

--- vim/lua/mapping.lua
+++ vim/lua/mapping.lua
@@ -82,7 +82,6 @@ map {'n', ';r', ':Rg<space>'}
 map {'n', ';x', ':Rg <c-r><c-w><cr>'}
 
 -- plugin fzf.lua
---[[
 map {'n', '<space>/', "<cmd>FzfLua search_history<cr>"}
 map {'n', '<space>;', "<cmd>FzfLua command_history<cr>"}
 map {'n', '<space>a', "<cmd>FzfLua lines<cr>"}
@@ -93,4 +92,3 @@ map {'n', '<space>j', "<cmd>FzfLua jumps<cr>"}
 map {'n', '<space>w', "<cmd>FzfLua grep_cword<cr>"}
 map {'n', '<space>x', "<cmd>FzfLua oldfiles<cr>"}
 vim.cmd[[nnoremap <space>c :FzfLua files cwd=<C-R>=expand("%:h")<cr><cr>]]
---]]
