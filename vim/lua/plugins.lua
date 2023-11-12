35af6f5a878bfaf9cf17beadb7268204eb360ca1
Wen-Long Yang <matesea@gmail.com>
2023-11-12 21:08:19 +0800

    feat: add mini.surround/mini.pairs

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -200,6 +200,7 @@ function M.setup()
             },
         },
 
+        --[[
         { 'windwp/nvim-autopairs',
             -- dependencies = 'nvim-treesitter',
             -- module = {'nvim-autopairs.completion.cmp', 'nvim-autopairs'},
@@ -207,6 +208,12 @@ function M.setup()
             config = function()
                 require('nvim-autopairs').setup()
             end
+        },
+        ]]
+
+        { 'echasnovski/mini.pairs',
+            event = 'InsertEnter',
+            opts = {}
         },
 
         { 'junegunn/fzf',
@@ -677,7 +684,40 @@ function M.setup()
             ft = 'scons',
         },
 
-        { "tpope/vim-surround" },
+        -- { "tpope/vim-surround" },
+        {
+            'echasnovski/mini.surround',
+            -- stylua: ignore
+            keys = function(_, keys)
+                -- Populate the keys based on the user's options
+                local plugin = require('lazy.core.config').spec.plugins['mini.surround']
+                local opts = require('lazy.core.plugin').values(plugin, 'opts', false)
+                local mappings = {
+                    { opts.mappings.add, desc = 'Add surrounding', mode = { 'n', 'x' } },
+                    { opts.mappings.delete, desc = 'Delete surrounding' },
+                    { opts.mappings.find, desc = 'Find right surrounding' },
+                    { opts.mappings.find_left, desc = 'Find left surrounding' },
+                    { opts.mappings.highlight, desc = 'Highlight surrounding' },
+                    { opts.mappings.replace, desc = 'Replace surrounding' },
+                    { opts.mappings.update_n_lines, desc = 'Update `MiniSurround.config.n_lines`' },
+                }
+                mappings = vim.tbl_filter(function(m)
+                    return m[1] and #m[1] > 0
+                end, mappings)
+                return vim.list_extend(mappings, keys)
+            end,
+            opts = {
+                mappings = {
+                    add = 'sa', -- Add surrounding in Normal and Visual modes
+                    delete = 'ds', -- Delete surrounding
+                    find = 'gzf', -- Find surrounding (to the right)
+                    find_left = 'gzF', -- Find surrounding (to the left)
+                    highlight = 'gzh', -- Highlight surrounding
+                    replace = 'cs', -- Replace surrounding
+                    update_n_lines = 'gzn', -- Update `n_lines`
+                },
+            },
+        },
 
         { 'neovim/nvim-lspconfig',
             lazy = true,
@@ -791,7 +831,20 @@ function M.setup()
                 -- either 'treesitte' or 'lspconfig'
                 require('config.nvim-ufo').setup('treesitter')
            end
-        }
+        },
+
+	    {
+	        'AndrewRadev/dsf.vim',
+	        -- stylua: ignore
+            lazy = true,
+	        keys = {
+	            { 'dsf', '<Plug>DsfDelete', noremap = true, desc = 'Delete Surrounding Function' },
+	            { 'csf', '<Plug>DsfChange', noremap = true, desc = 'Change Surrounding Function' },
+	        },
+	        init = function()
+	            vim.g.dsf_no_mappings = 1
+	        end,
+	    },
         --[[
 
         { 'skywind3000/vim-preview',
