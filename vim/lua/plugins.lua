e319494c2d6992672970fc01513a81b2aca17982
Wen-Long Yang <matesea@gmail.com>
2024-07-24 16:23:34 +0800

    vimrc: add mini.align

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -1126,6 +1126,20 @@ function M.setup()
             end,
         },
 
+        {
+            'echasnovski/mini.align',
+            opts = {
+                mappings = {
+                    start = 'gb',
+                    start_with_preview = 'gB',
+                },
+            },
+            keys = {
+                { 'gb', mode = { 'n', 'x' } },
+                { 'gB', mode = { 'n', 'x' } },
+            },
+        },
+
     }
 
     if ver.major >= 1 or ver.minor >= 9 then
