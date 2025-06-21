de7b19cd454860e206f4aec631458f0f871e0420
Wen-Long Yang <matesea@gmail.com>
2025-06-21 17:01:33 +0800

    vimrc: update indent plugins

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -283,6 +283,24 @@ function M.setup()
              end
         },
 
+        { 'nathanaelkane/vim-indent-guides',
+            lazy = true,
+            enabled = false;
+            ft = ft_code,
+            init = function()
+                vim.g.indent_guides_enable_on_vim_startup = 1
+                vim.g.indent_guides_default_mapping = 0
+                vim.g.indent_guides_tab_guides = 0
+                vim.g.indent_guides_color_change_percent = 3
+                vim.g.indent_guides_guide_size = 1
+                vim.g.indent_guides_exclude_filetypes = {
+                    'help', 'defx', 'denite', 'denite-filter', 'startify',
+                    'vista', 'vista_kind', 'tagbar', 'lsp-hover', 'clap_input',
+                    'any-jump', 'gina-status', 'gina-commit', 'gina-log', 'terminal',
+                    'fzf'
+                }
+            end
+        },
     }
     ]]
 
@@ -417,7 +435,6 @@ function M.setup()
             ft = 'qf',
             keys = {
                 {'<leader>q', '<cmd>QFToggle!<cr>', desc = 'toggle quickfix'},
-                {'<leader>f', '<cmd>LLToggle!<cr>', desc = 'toggle location list'},
                 {']q', '<cmd>QNext<cr>', desc = 'next quickfix/location list item'},
                 {'[q', '<cmd>QPrev<cr>', desc = 'previous quickfix/location list item'},
             },
@@ -603,22 +620,22 @@ function M.setup()
             },
         },
 
-        { 'nathanaelkane/vim-indent-guides',
+        { 'echasnovski/mini.indentscope',
+            version = '*',
             lazy = true,
             ft = ft_code,
-            init = function()
-                vim.g.indent_guides_enable_on_vim_startup = 1
-                vim.g.indent_guides_default_mapping = 0
-                vim.g.indent_guides_tab_guides = 0
-                vim.g.indent_guides_color_change_percent = 3
-                vim.g.indent_guides_guide_size = 1
-                vim.g.indent_guides_exclude_filetypes = {
-                    'help', 'defx', 'denite', 'denite-filter', 'startify',
-                    'vista', 'vista_kind', 'tagbar', 'lsp-hover', 'clap_input',
-                    'any-jump', 'gina-status', 'gina-commit', 'gina-log', 'terminal',
-                    'fzf'
-                }
+            config = function()
+                require('mini.indentscope').setup()
             end
+        },
+
+        {  "lukas-reineke/indent-blankline.nvim",
+            lazy = true,
+            ft = ft_code,
+            main = "ibl",
+            ---@module "ibl"
+                ---@type ibl.config
+            opts = {},
         },
 
         { 'dstein64/vim-startuptime',
