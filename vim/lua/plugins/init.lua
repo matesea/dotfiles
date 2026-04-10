3accd08c4cc8cf5c3df36d1de4b0ff7e0659ebc9
Wen-Long Yang <matesea@gmail.com>
2026-04-10 15:06:46 +0800

    vimrc: launch lspconfig only for lua filetype

--- vim/lua/plugins/init.lua
+++ vim/lua/plugins/init.lua
@@ -1106,7 +1106,7 @@ local plugins = {
 		-- Main LSP Configuration
 		"neovim/nvim-lspconfig",
 		lazy = true,
-		ft = ft_code,
+		ft = 'lua',
 		dependencies = {
 			-- Automatically install LSPs and related tools to stdpath for Neovim
 			-- Mason must be loaded before its dependents so we need to set it up here.
